bc4eeead6b1668fb24d7f7d5453334a33a8bdebd
robertslabriab <52362750+robertslabriab@users.noreply.github.com>
2019-11-17 23:04:13 -0500

    added progress and shippingInvoiceInput

---
+++ ShippingInvoiceInput.java
@@ -0,0 +1,28 @@
+//javac -cp .;junit-cs211.jar *.java
+//java -cp .;junit-cs211.jar ShippingInvoiceInput
+
+import javax.swing.*;
+public class ShippingInvoiceInput{
+    
+    public ShippingInvoiceInput(){}
+
+    public static void InvoiceInputDisplay(){
+        JFrame InvoiceFrame = new JFrame("Invoice Information");
+        InvoiceFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        InvoiceFrame.setSize(300,300);
+        String name=JOptionPane.showInputDialog(InvoiceFrame,"Customer Name");
+        String address=JOptionPane.showInputDialog(InvoiceFrame,"Customer Address");
+        String phoneNum=JOptionPane.showInputDialog(InvoiceFrame,"Customer Phone Number");
+        String email=JOptionPane.showInputDialog(InvoiceFrame,"Customer Email");
+        String product=JOptionPane.showInputDialog(InvoiceFrame,"Product");
+        String productSerialNum=JOptionPane.showInputDialog(InvoiceFrame,"Product Serial Number");
+        String quantity=JOptionPane.showInputDialog(InvoiceFrame,"Quantity");
+        String unitPrice=JOptionPane.showInputDialog(InvoiceFrame,"Unit Price");
+        String comments=JOptionPane.showInputDialog(InvoiceFrame,"Comments/Notes");
+        InvoiceFrame.setVisible(true);
+    }
+
+    public static void main(String[]args){
+        new ShippingInvoiceInput();
+    }
+}
